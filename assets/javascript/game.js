0b1150d0e55f22b1050c25ff0d9be2178aa4424c
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-02-27 03:18:03 -0500

    Format HTML; add guessesToString and updateGameBoard

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 // starting variables
-var computerLetter;
-var playerGuesses;
-var playerGuessesString;
+var computerLetter = '';
+var playerGuesses = "";
+// var playerGuessesString = "";
 var wins = 0;
 var losses = 0;
+var guessesLeft = 9;
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
@@ -23,5 +24,26 @@ function randomLetter() {
 
 // game initialization
 
+// Formats player guesses (lowercase) to be uppercase and have separating commas
+function guessesToString(guesses) {
+    var guessString = "";
+    for (var k = 0; k < guesses.length; k++) {
+        guessString += guesses[k];
+        // don't add a terminal comma
+        if (k < guesses.length-1) {
+            guessString += ', '
+        }
+    }
+    return guessString.toUpperCase();
+ 
+}
 
+// To be called after a player makes a guess
+function updateGameBoard() {
+    spanWins.innerHTML = wins;
+    spanLosses.innerHTML = losses;
+    spanGuessesLeft.innerHTML = guessesLeft;
+    spanGuesses.innerHTML = guessesToString(playerGuesses);
+    // spanGuesses.innerHTML = "guesses go here"; // guessesToString(guesses);
 
+}
